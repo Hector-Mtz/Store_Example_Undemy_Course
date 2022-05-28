3d290444070e72edc4246d8ebe580b76fcd17842
Hector Hugo Martinez Martinez <57997735+hh-mm77@users.noreply.github.com>
2022-05-28 11:52:21 -0500

    Finalizado

--- producto.php
+++ producto.php
@@ -25,7 +25,18 @@
          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
            Adipisci consequatur dolor consectetur asperiores? Nam dolorem, 
            neque mollitia quibusdam repudiandae ab blanditiis provident quis pariatur, 
-           reprehenderit error perferendis? Veritatis, doloremque. Sapiente.</p>
+           reprehenderit error perferendis? Veritatis, doloremque. Sapiente.
+          </p>
+          <form class="formulario">
+              <select class="formulario__campo" name="" id="">
+                <option value="" selected disabled>Selecciona una talla</option>
+                <option value="">Chica</option>
+                <option value="">Mediana</option>
+                <option value="">Grande</option>
+              </select>
+              <input class="formulario__campo"  type="number" placeholder="Cantidad" min="1" step="10">
+              <input class="formulario__submit"  type="submit" value="Agregar a carrito">
+          </form>
       </div>
    </div>
 
